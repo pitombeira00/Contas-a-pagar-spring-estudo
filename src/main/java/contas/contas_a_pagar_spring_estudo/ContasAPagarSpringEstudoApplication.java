5f4fb2fad29295c7e664a267d6a534b547133000
Danilo Pitombeira <danilo_pitombera@hotmail.com>
2024-07-29 20:19:00 -0300

    Feat: Commit Init

---
+++ src/main/java/contas/contas_a_pagar_spring_estudo/ContasAPagarSpringEstudoApplication.java
@@ -0,0 +1,13 @@
+package contas.contas_a_pagar_spring_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasAPagarSpringEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasAPagarSpringEstudoApplication.class, args);
+	}
+
+}
